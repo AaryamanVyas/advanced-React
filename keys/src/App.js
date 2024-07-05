e44447a053ded60b5435b79575d29dc53086a767
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-07-05 17:51:49 +0000

    final

--- keys/src/App.js
+++ keys/src/App.js
@@ -1,39 +1,40 @@
 import './index.css';
-import {useState} from "react";
+import { useState } from "react";
+
 const ToDo = props => (
   <tr>
     <td>
-|     <label>{props.id}</label>
+      <label>{props.id}</label>
     </td>
-  <td>
-| <input />
-  </td>
-<td>
-| <label>{props.createdAt}</label>
-</td>
-</tr>
+    <td>
+      <input />
+    </td>
+    <td>
+      <label>{props.createdAt}</label>
+    </td>
+  </tr>
 );
 
 function App() {
   const [todos, setTodos] = useState([{
-    id: 'todol',
+    id: 'todo1',
     createdAt: '18:00',
   }, {
     id: 'todo2',
     createdAt: '20:30',
   }]);
+
   const reverseOrder = () => {
-    
-    setTodos ([...todos].reverse());
+    setTodos([...todos].reverse());
   }
-  
-  return(
+
+  return (
     <div>
       <button onClick={reverseOrder}> Reverse</button>
       <table>
         <tbody>
           {todos.map((todo, index) => (
-            <ToDo id={todo.id} createdAt={todo.createdAt} />
+            <ToDo key={index} id={todo.id} createdAt={todo.createdAt} />
           ))}
         </tbody>
       </table>
