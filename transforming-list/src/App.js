256413dfd8a645b24b7b9322ee3654a34ead2521
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-07-05 13:39:06 +0000

    final

--- transforming-list/src/App.js
+++ transforming-list/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+  {
+    id: "1",
+    title: "Tiramisu",
+    description: "The best tiramisu in town",
+    image: "https://picsum.photos/200/300/?random",
+    price: "$5.00",
+  },
+  {
+    id: "2",
+    title: "Lemon Ice Cream",
+    description: "Mind blowing taste",
+    image: "https://picsum.photos/200/300/?random",
+    price: "$4.50",
+  },
+  {
+    id: "3",
+    title: "Chocolate mousse",
+    description: "Unexplored flavour",
+    image: "https://picsum.photos/200/300/?random",
+    price: "$6.00",
+  },
+];
+const topDesserts =data.map(dessert => {
+  return{
+    content: `${dessert.title} - ${dessert.description}`,
+    price: dessert.price,
+  }
+})
+export default function App() {
+  console.log(topDesserts);
+  return <h1>Examine the console output</h1>;
 }
-
-export default App;
