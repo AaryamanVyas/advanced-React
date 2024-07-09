d9351fe714320c7ee56f815503f52a596dec4062
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-07-09 16:09:38 +0000

    initial form

--- controlled-forms/src/App.js
+++ controlled-forms/src/App.js
@@ -4,20 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <fieldset>
+          <h2>Feedback form</h2>
+          <button type='submit'>Submit</button>
+        </fieldset>
+      </form>
     </div>
   );
 }
