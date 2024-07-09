383822b2e2903ca77152f5663f67ef0f15da591b
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-07-09 16:39:42 +0000

    final

--- controlled-forms/src/App.js
+++ controlled-forms/src/App.js
@@ -3,9 +3,20 @@ import { useState } from 'react';
 
 function App() {
   const [score, setScore] = useState("10");
+  const [comment, setComment] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (Number(score) <= 5 && comment.length <= 10) {
+      alert("Please provide a comment explaining why the experience was poor");
+      return;
+    }
+
+    console.log("Form submitted");
+    setScore("10");
+  }
   return (
     <div className="App">
-      <form>
+      <form onSubmit={handleSubmit}>
         <fieldset>
           <h2>Feedback form</h2>
           <div className='Field'>
@@ -15,7 +26,12 @@ function App() {
               min="0" 
               max="10" 
               value={score} 
-              onChange={e => setScore(e.target.value)} />
+              onChange={e => setScore(e.target.value)} 
+            />
+          </div>
+          <div className='Field'>
+            <label>Comment:</label>
+            <textarea value={comment} onChange={e => setComment(e.target.value)} />
           </div>
           <button type='submit'>Submit</button>
         </fieldset>
