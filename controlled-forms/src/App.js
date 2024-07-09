6473319a92c325cdeadf008a8e25ae22c0129a83
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-07-09 16:11:40 +0000

    created slider

--- controlled-forms/src/App.js
+++ controlled-forms/src/App.js
@@ -7,6 +7,10 @@ function App() {
       <form>
         <fieldset>
           <h2>Feedback form</h2>
+          <div className='Field'>
+            <label>Score: </label>
+            <input type='range' min="0" max="10" />
+          </div>
           <button type='submit'>Submit</button>
         </fieldset>
       </form>
