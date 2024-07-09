4b997a8e233a43098ee8fa770836c6d600fd5c47
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-07-09 16:20:12 +0000

    adding numbers according to the scroll

--- controlled-forms/src/App.js
+++ controlled-forms/src/App.js
@@ -1,15 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [score, setScore] = useState("10");
   return (
     <div className="App">
       <form>
         <fieldset>
           <h2>Feedback form</h2>
           <div className='Field'>
-            <label>Score: </label>
-            <input type='range' min="0" max="10" />
+            <label>Score: {score}⭐</label>
+            <input 
+              type='range' 
+              min="0" 
+              max="10" 
+              value={score} 
+              onChange={e => setScore(e.target.value)} />
           </div>
           <button type='submit'>Submit</button>
         </fieldset>
