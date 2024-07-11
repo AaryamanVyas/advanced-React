f106c9f632a72ddbc6a67649c62a76b938519334
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-07-11 12:41:29 +0000

    bugfix

--- context/src/Usercontext.js
+++ context/src/Usercontext.js
@@ -1,13 +1,15 @@
-import { Children, createContext, useContext, useState } from "react";
- const UserContext = createContext(undefined);
+import { createContext, useContext, useState } from "react";
 
-export const UserProvider = ({children}) => {
-    const [user] = useState({
-        name:"John",
-        email:"John@example.com",
-        dob:"01/01/2000",
-    });
-    return <UserContext.Provider value={{ user }}></UserContext.Provider>
+const UserContext = createContext(undefined);
+
+export const UserProvider = ({ children }) => {
+  const [user] = useState({
+    name: "John",
+    email: "John@example.com",
+    dob: "01/01/2000",
+  });
+
+  return <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>;
 };
 
 export const useUser = () => useContext(UserContext);
