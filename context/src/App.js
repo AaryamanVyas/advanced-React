3db450fd38763a13220c1aef39c510494c8a4f2a
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-07-11 12:20:14 +0000

    initial

--- context/src/App.js
+++ context/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
+const LoggedInUser = () => {
+  return (
+    <p>
+      Hello <span className='Username'></span>
+    </p>
+  );
+};
+
+const Header = () => {
+  return(
+    <header>
+      <h2>Blog App</h2>
+      <LoggedInUser />
+    </header>
+  )
+}
+
+const Page = () => {
+  return (
+    <div>
+      <h2>What is Lorem ipsum</h2>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+      occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+      mollit anim id est laborum,
+      </p>
+      <p>Written by </p>
+    </div>
+  )
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <page />
     </div>
   );
 }
-
-export default App;
+function Root() {
+  return (
+    <UserProvider>
+      <App />
+    </UserProvider>
+  )
+}
