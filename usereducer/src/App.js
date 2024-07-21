0abc2c52ae1c08ee9bf1ba5f4b0fe9be85ec96ab
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-07-21 13:56:54 +0000

    bugfixes pt2

--- usereducer/src/App.js
+++ usereducer/src/App.js
@@ -1,23 +1,22 @@
 import { useReducer } from 'react';
 import './App.css';
 
-
 const reducer = (state, action) => {
-  if (action.type === 'buy_ingredients') return {money: state.money - 10};
-  if (action.type === 'sell_A_meal') return {money: state.money + 10};
+  if (action.type === 'buy_ingredients') return { money: state.money - 10 };
+  if (action.type === 'sell_A_meal') return { money: state.money + 10 };
   return state;
-}
-function App() {
+};
 
-  const initialState = {money: 100};
+function App() {
+  const initialState = { money: 100 };
   const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
     <div className='App'>
       <h1>Wallet: {state.money}</h1>
       <div>
-        <button onClick={() => dispatch({type: 'buy_ingredients'})}>Shopping for veggies!</button>
-        <button onClick={() => dispatch({type: "sell_a_meal"})}>serve a meal to the customer</button>
+        <button onClick={() => dispatch({ type: 'buy_ingredients' })}>Shopping for veggies!</button>
+        <button onClick={() => dispatch({ type: 'sell_A_meal' })}>Serve a meal to the customer</button>
       </div>
     </div>
   );
