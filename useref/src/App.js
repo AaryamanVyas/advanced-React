cad420d245ab1b530b533fd49175b1b3bb4fcdcd
aaryamantriescode <aaryamanvyas1@gmail.com>
2024-07-21 14:19:46 +0000

    const focusInput

--- useref/src/App.js
+++ useref/src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const focusInput = () => {
+    formInputRef.current.focus();
+  }
+
   return (
     <>
     <h1>Using useRef to access underlying DOM</h1>
